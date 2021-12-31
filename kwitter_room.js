bf621adc64009ed15f2e769960b326fc22f52078
KBNfi <90031819+KBNfi@users.noreply.github.com>
2021-12-31 14:56:35 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,53 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+      apiKey: "AIzaSyCzfRy87sFDk6MH4GiKscxuegdCh5plvzU",
+      authDomain: "kwitter-2-1be6a.firebaseapp.com",
+      projectId: "kwitter-2-1be6a",
+      storageBucket: "kwitter-2-1be6a.appspot.com",
+      messagingSenderId: "553023091098",
+      appId: "1:553023091098:web:a45e5ea87203de10738a23",
+      databaseURL: "https://kwitter-2-1be6a-default-rtdb.firebaseio.com/"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+function addRoom() {
+      room_name = document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update({
+            purpose: "Adding Room Name"
+      });
+      localStorage.setItem("room_name", room_name);
+      window.location = "kwitter_page.html";
+}
+
+function getData() {
+      firebase.database().ref("/").on('value', function (snapshot) {
+            document.getElementById("output").innerHTML = ""; snapshot.forEach(function (childSnapshot) {
+                  childKey = childSnapshot.key;
+                  Room_names = childKey;
+                  //Start code
+                  console.log("Room Name - " + Room_names);
+                  row = "<div class='room_name' id=" + Room_names + "onclick='redirectToRoomName(this.id)'>#" + Room_names + "</div><hr>";
+                  document.getElementById("output").innerHTML += row;
+
+                  //End code
+            });
+      });
+}
+getData();
+
+function redirectToRoomName(name) {
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
+
+function logout() {
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = index.html;
+
+}
